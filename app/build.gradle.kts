7008ea01c913e21f187d3775dfcb7a6196ff1185
xxx <xxx@debian>
2026-08-15 14:31:12 -0400

    chore(0.1.8): bump versionCode 8 / versionName 0.1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,8 +96,8 @@ android {
         applicationId = "com.mindchat.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.1.7"
+        versionCode = 8
+        versionName = "0.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
